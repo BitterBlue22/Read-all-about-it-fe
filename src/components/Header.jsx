a136d9631651aa77689f544479d9e1ec4e7f0648
BitterBlue22 <telisax@gmail.com>
2020-05-27 20:36:02 +0100

    updated header to let title show

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ user: { username, avatar_url } }) => {
   return (
     <header className="App-header">
-      <title>NC News</title>
+      <h1>NC News</h1>
       <p>Signed in as: {username}</p>
       <img src={avatar_url} alt="Avatar" className="user-avatar" />
     </header>

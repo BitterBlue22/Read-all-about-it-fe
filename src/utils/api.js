4265e4c57c8fba083c1699fc0792ca6ffd58c80f
BitterBlue22 <telisax@gmail.com>
2020-05-27 20:42:40 +0100

    updated utils file

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,10 +8,14 @@ export const fetchAllTopics = () => {
     });
 };
 
-export const fetchAllArticles = () => {
-  return axios.get().then(({ data: { articles } }) => {
-    return articles;
-  });
+export const fetchAllArticles = (topic) => {
+  return axios
+    .get("https://tel-news-port.herokuapp.com/api/articles", {
+      params: { topic },
+    })
+    .then(({ data: { articles } }) => {
+      return articles;
+    });
 };
 
 export const fetchAllUsers = () => {
@@ -19,5 +23,34 @@ export const fetchAllUsers = () => {
     .get("https://tel-news-port.herokuapp.com/api/users")
     .then(({ data: { users } }) => {
       return users;
+    });
+};
+
+export const patchVotes = (article_id) => {
+  return axios.patch(
+    `https://tel-news-port.herokuapp.com/api/articles/${article_id}`,
+    {
+      inc_votes: 1,
+    }
+  );
+};
+
+export const fetchArticleById = (article_id) => {
+  return axios
+    .get(`https://tel-news-port.herokuapp.com/api/articles/${article_id}`)
+    .then(({ data: { article } }) => {
+      return article;
+    });
+};
+
+export const postCommentByArticleId = (article_id) => {};
+
+export const fetchAllComments = (article_id) => {
+  return axios
+    .get(
+      `https://tel-news-port.herokuapp.com/api/articles/${article_id}/comments`
+    )
+    .then(({ data: { comments } }) => {
+      return comments;
     });
 };

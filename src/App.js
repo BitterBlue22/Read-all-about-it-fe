f2557da801eea2ac863983e2dd244c97d2f95d44
BitterBlue22 <telisax@gmail.com>
2020-05-29 13:15:27 +0100

    fixed topic_slug link to sort articles by topic

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ import Topics from "./components/Topics";
 import Articles from "./components/Articles";
 import Users from "./components/Users";
 import Article from "./components/Article";
+import CommentsList from "./components/CommentsList";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faCheckSquare,
+  faSpinner,
+  faHome,
+  faNewspaper,
+  faUsers,
+  faBoxes,
+  faUser,
+  faClock,
+  faTrash,
+  faComment,
+  faComments,
+  faThumbsDown,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 class App extends Component {
   state = {
     user: {
-      username: "Zizi",
+      username: "weegembump",
       avatar_url:
         "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f1411f51-b876-4331-9363-f895deabea7f/db3lx36-28f54f1a-46bf-4d0a-a99e-31fc03733c2d.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZjE0MTFmNTEtYjg3Ni00MzMxLTkzNjMtZjg5NWRlYWJlYTdmXC9kYjNseDM2LTI4ZjU0ZjFhLTQ2YmYtNGQwYS1hOTllLTMxZmMwMzczM2MyZC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.8yyAxnaqfxrdbK5mLdLamkNmq4Cxeho52Yxfe26zxWQ",
     },
@@ -26,22 +44,41 @@ class App extends Component {
         <Router className="Router">
           <Topics path="/topics" />
           <Articles path="/articles" />
-          <Article path="/articles/:article_id" />
+          <Articles path="topics/articles" />
+          <Article
+            path="/articles/:article_id"
+            user={this.state.user.username}
+          />
           <Articles path="/articles/:article_id/comments" />
           <Users path="/users" />
           <Articles path="/:topic_slug" />
+          <CommentsList
+            path="/comments/:comment_id"
+            user={this.state.user.username}
+          />
         </Router>
         <section>
           <h2 className="welcome-text">Welcome to NC news!</h2>
         </section>
-
         <Footer />
       </div>
     );
   }
 }
-//
+library.add(
+  faCheckSquare,
+  faSpinner,
+  faHome,
+  faNewspaper,
+  faUsers,
+  faBoxes,
+  faUser,
+  faClock,
+  faTrash,
+  faComment,
+  faComments,
+  faThumbsDown,
+  faThumbsUp
+);
 
 export default App;
-
-//https://tel-news-port.herokuapp.com/api
